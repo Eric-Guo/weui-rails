81015d66dac34192a63c420bdff35a5e115c60ff
Eric Guo <eric.guo@sandisk.com>
2016-01-10 15:49:13 +0800

    Allow suppress icon style and button_text.

--- lib/weui-rails/helpers/weui_message_box.rb
+++ lib/weui-rails/helpers/weui_message_box.rb
@@ -1,6 +1,6 @@
 module Weui
   module Helpers
-    def weui_message_box(title, description, icon_style, button_text = '确定')
+    def weui_message_box(title, description, icon_style = 'weui_icon_warn', button_text = '确定')
       render partial: 'weui/message_box',
              locals: { title: title, description: description,
                        icon_style: icon_style, button_text: button_text }
